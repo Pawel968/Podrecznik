8261fc71fb9b793c8505d29d786897fe37fcb1d0
Pawel968 <cabanpawel96@gmail.com>
2025-05-09 12:34:04 +0200

    Początek pracy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ function App() {
 
   return (
     <>
-      
+      <h1>Podręcznik elektroniczny w języku HTML</h1>
+      <br/>
+      <section>
+      <a href="">Tekst</a>
+      <a href="">Listy</a>
+      <a href="">Tabele</a>
+      </section>
+      <section>
+      <h2>O podręczniku elektronicznym</h2>
+      <p>Podręcznik elektroniczny, czyli e-podręcznik, to cyfrowa wersja tradycyjnego podręcznika wykorzystywana w edukacji. 
+        Może być dostępny online lub w formie pliku (np. PDF, EPUB), który można pobrać na komputer, tablet czy smartfon. 
+        E-podręczniki zawierają te same treści co ich papierowe odpowiedniki, ale często wzbogacone są o dodatkowe funkcje multimedialne.</p>
+      </section>
     </>
   )
 }
